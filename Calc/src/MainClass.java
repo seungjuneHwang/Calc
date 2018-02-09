c1b9654d1c2c3aba809c035b694d181276c2ae51
Administrator <Administrator@192.168.5.12>
2018-02-09 16:06:07 +0900

    메인 클래스에 주석 추가

--- Calc/src/MainClass.java
+++ Calc/src/MainClass.java
@@ -6,6 +6,9 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// 더하기를 클래스를 생성을 해서 넣으면 더하기
+		// 빼기 클래스를 생성을 해서 넣으면 빼기
+		
 		OperClass oc = new OperClass();
 		oc.inputNumber();  // 두개의 숫자를 입력
 		oc.printResult();
