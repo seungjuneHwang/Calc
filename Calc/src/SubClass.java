1024af74c890bb9efd9e14c70eb9af61fc5c213e
Administrator <Administrator@192.168.5.12>
2018-02-09 11:07:35 +0900

    SubClass 수정

--- Calc/src/SubClass.java
+++ Calc/src/SubClass.java
@@ -3,7 +3,7 @@ public class SubClass implements Hab {
 	@Override
 	public void clac(NumVo nc) {
 		
-		System.out.println("빼기 연산");
+		System.out.println("빼기 연산ㅁㅁㅁㅁㅁ");
 		int num1 = nc.getNum1();
 		int num2 = nc.getNum2();
 		System.out.println(num1 + "-" + num2 + "=" 
